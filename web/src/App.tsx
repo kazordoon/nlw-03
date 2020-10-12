ff0e1a8bdab439a88e2dd708da59deed8a24b4cc
Felipe Barros <felipetech@pm.me>
2020-10-12 14:41:16 -0300

    refactor(web): Move landing page to its own file

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,30 +1,8 @@
 import React from 'react';
-import { FiArrowRight } from 'react-icons/fi';
-import logoImg from './assets/images/logo.svg';
-import './assets/styles/pages/landing.css';
+import Landing from './pages/Landing';
 
 function App() {
-  return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="logo" />
-
-        <main>
-          <h1>Leve a felicidade para o mundo.</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-        </main>
-
-        <div className="location">
-          <strong>Salvador</strong>
-          <span>Bahia</span>
-        </div>
-
-        <a href="#" className="enter-app">
-          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
-        </a>
-      </div>
-    </div>
-  );
+  return <Landing />;
 }
 
 export default App;
---
+++ web/src/pages/Landing.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+import logoImg from '../assets/images/logo.svg';
+import '../assets/styles/pages/landing.css';
+
+const Landing: React.FC = () => (
+  <div id="page-landing">
+    <div className="content-wrapper">
+      <img src={logoImg} alt="logo" />
+
+      <main>
+        <h1>Leve a felicidade para o mundo.</h1>
+        <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+      </main>
+
+      <div className="location">
+        <strong>Salvador</strong>
+        <span>Bahia</span>
+      </div>
+
+      <a href="#" className="enter-app">
+        <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+      </a>
+    </div>
+  </div>
+);
+
+export default Landing;
