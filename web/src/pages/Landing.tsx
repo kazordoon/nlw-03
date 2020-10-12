4353790fa40b5b3b77ef6e688c56244d58900b83
Felipe Barros <felipetech@pm.me>
2020-10-12 14:57:19 -0300

    chore(web): Add routes

--- web/src/pages/Landing.tsx
+++ web/src/pages/Landing.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { FiArrowRight } from 'react-icons/fi';
 import logoImg from '../assets/images/logo.svg';
 import '../assets/styles/pages/landing.css';
+import { Link } from 'react-router-dom';
 
 const Landing: React.FC = () => (
   <div id="page-landing">
@@ -18,9 +19,9 @@ const Landing: React.FC = () => (
         <span>Bahia</span>
       </div>
 
-      <a href="#" className="enter-app">
+      <Link to="/orphanages" className="enter-app">
         <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
-      </a>
+      </Link>
     </div>
   </div>
 );
---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const OrphanagesMap: React.FC = () => <h1>Hello World</h1>;
+
+export default OrphanagesMap;
