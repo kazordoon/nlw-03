77f3c2b59e3ff3d4355f194067e2a4250cc3f3af
Felipe Barros <felipetech@pm.me>
2020-10-13 13:03:19 -0300

    chore(server): Add database connection

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EnvironmentVariablesModule } from './environment-variables/environment-variables.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [EnvironmentVariablesModule],
+  imports: [EnvironmentVariablesModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
