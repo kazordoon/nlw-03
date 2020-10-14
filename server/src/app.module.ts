a17078024b389cf9398ddffcbc1b1927123ee07f
Felipe Barros <felipetech@pm.me>
2020-10-14 11:39:59 -0300

    refactor(server): Remove unused code

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EnvironmentVariablesModule } from './environment-variables/environment-variables.module';
 import { DatabaseModule } from './database/database.module';
 import { OrphanagesModule } from './orphanages/orphanages.module';
 
 @Module({
   imports: [EnvironmentVariablesModule, DatabaseModule, OrphanagesModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
