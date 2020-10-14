4fa2e4769b7677bc96320371463cf4923b681899
Felipe Barros <felipetech@pm.me>
2020-10-13 23:27:54 -0300

    chore(server): Add orphanage DTO

---
+++ server/src/orphanages/dto/create-orphanage.dto.ts
@@ -0,0 +1,15 @@
+export class CreateOrphanageDTO {
+  public name: string;
+
+  public latitude: number;
+
+  public longitude: number;
+
+  public about: string;
+
+  public instructions: string;
+
+  public open_on_weekends: boolean;
+
+  public opening_hours: string;
+}
