5b97de667a211be7560b9415eee2d6fd328fe73f
Felipe Barros <felipetech@pm.me>
2020-10-15 11:40:33 -0300

    chore(web): Add custom map marker

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import Leaflet from 'leaflet';
+
 import 'leaflet/dist/leaflet.css';
 import mapMarkerImg from '../assets/images/map-marker.svg';
 import '../assets/styles/pages/orphanages-map.css';
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
 
 const OrphanagesMap: React.FC = () => (
   <div id="page-map">
@@ -28,6 +37,19 @@ const OrphanagesMap: React.FC = () => (
       style={{ width: '100%', height: '100%' }}
     >
       <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <Marker position={[-12.9799063, -38.5115833]} icon={mapIcon}>
+        <Popup
+          closeButton={false}
+          minWidth={240}
+          maxWidth={240}
+          className="map-popup"
+        >
+          Nome
+          <Link to="">
+            <FiArrowRight size={20} color="#fff" />
+          </Link>
+        </Popup>
+      </Marker>
     </Map>
 
     <div>
