b27ebba33e3314230a8e1a8f7bf3094641e3d086
Felipe Barros <felipetech@pm.me>
2020-10-16 00:23:14 -0300

    chore(mobile): Add mobile structure

---
+++ mobile/App.tsx
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello World</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#444',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: '#e1e1e1',
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+});
