9fedbd773a6b5965efccbb37179dd874f46474bb
Felipe Barros <felipetech@tuta.io>
2020-10-17 13:27:19 -0300

    refactor(mobile): Move fonts loading to App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import {
+  Nunito_600SemiBold,
+  Nunito_700Bold,
+  Nunito_800ExtraBold,
+} from '@expo-google-fonts/nunito';
 
 import Routes from './src/routes';
 
 export default function App() {
+  const [fontsHaveBeenLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold,
+  });
+
+  const fontsHaveNotBeenLoaded = !fontsHaveBeenLoaded;
+
+  if (fontsHaveNotBeenLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Routes />
