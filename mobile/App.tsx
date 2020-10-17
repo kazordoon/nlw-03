d5f41075bf2879128bf5b5a38831293cd22e161e
Felipe Barros <felipetech@tuta.io>
2020-10-17 13:14:07 -0300

    refactor(mobile): Rename OrphanagesMap.tsx to index.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
-import OrphanagesMap from './src/pages/OrphanagesMap/OrphanagesMap';
+import OrphanagesMap from './src/pages/OrphanagesMap';
 
 export default function App() {
   return (
