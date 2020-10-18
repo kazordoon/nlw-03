b0db77f9a3b1129e34fc42999cfb9ec269f6effd
Felipe Barros <felipetech@tuta.io>
2020-10-18 19:13:49 -0300

    chore(mobile): Add Header component

---
+++ mobile/src/components/Header/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { BorderlessButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
+import styles from './styles';
+import HeaderProps from '../../contracts/HeaderProps';
+
+const Header: React.FC<HeaderProps> = ({ title, showCancel = true }) => {
+  const navigation = useNavigation();
+
+  function handleNavigationToHomepage() {
+    navigation.navigate('OrphanagesMap');
+  }
+
+  return (
+    <View style={styles.container}>
+      <BorderlessButton onPress={navigation.goBack}>
+        <Feather name="arrow-left" size={24} color="#15b6d6" />
+      </BorderlessButton>
+
+      <Text style={styles.title}>{title}</Text>
+
+      {showCancel ? (
+        <BorderlessButton onPress={handleNavigationToHomepage}>
+          <Feather name="x" size={24} color="#ff669d" />
+        </BorderlessButton>
+      ) : (
+        <View />
+      )}
+    </View>
+  );
+};
+
+export default Header;
---
+++ mobile/src/components/Header/styles.ts
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    padding: 24,
+    paddingTop: 44,
+
+    backgroundColor: '#f9fafc',
+
+    borderBottomWidth: 1,
+    borderColor: '#dde3f0',
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  title: {
+    fontFamily: 'Nunito_600SemiBold',
+    color: '#8fa7b3',
+    fontSize: 16,
+  },
+});
