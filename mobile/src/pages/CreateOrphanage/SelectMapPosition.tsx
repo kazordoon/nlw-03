c5e2cd0f96b268191e1fb048142a090ce3fe4538
Felipe Barros <felipetech@tuta.io>
2020-10-19 11:09:59 -0300

    chore(mobile): Finish the position selection on the map
    Finish the position selection on the map when registering a new orphanage

--- mobile/src/pages/CreateOrphanage/SelectMapPosition.tsx
+++ mobile/src/pages/CreateOrphanage/SelectMapPosition.tsx
@@ -1,39 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, Dimensions, Text } from 'react-native';
-
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, MapEvent } from 'react-native-maps';
 
 import mapMarkerImg from '../../../assets/images/map-marker.png';
 
 export default function SelectMapPosition() {
   const navigation = useNavigation();
 
+  const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
+  const [
+    hasThePositionBeenDefined,
+    setWhetherThePositionHasBeenDefined,
+  ] = useState(false);
+
   function handleNextStep() {
-    navigation.navigate('OrphanageData');
+    navigation.navigate('OrphanageData', { position });
+  }
+
+  function handleSelectionOfMapPosition(event: MapEvent) {
+    setPosition(event.nativeEvent.coordinate);
+    setWhetherThePositionHasBeenDefined(true);
   }
 
   return (
     <View style={styles.container}>
       <MapView
         initialRegion={{
-          latitude: -27.2092052,
-          longitude: -49.6401092,
+          ...position,
           latitudeDelta: 0.008,
           longitudeDelta: 0.008,
         }}
         style={styles.mapStyle}
+        onPress={handleSelectionOfMapPosition}
       >
-        <Marker
-          icon={mapMarkerImg}
-          coordinate={{ latitude: -27.2092052, longitude: -49.6401092 }}
-        />
+        {hasThePositionBeenDefined && (
+          <Marker icon={mapMarkerImg} coordinate={position} />
+        )}
       </MapView>
 
-      <RectButton style={styles.nextButton} onPress={handleNextStep}>
-        <Text style={styles.nextButtonText}>Próximo</Text>
-      </RectButton>
+      {hasThePositionBeenDefined && (
+        <RectButton style={styles.nextButton} onPress={handleNextStep}>
+          <Text style={styles.nextButtonText}>Próximo</Text>
+        </RectButton>
+      )}
     </View>
   );
 }
