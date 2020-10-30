51be4f19a861f5e541acfb4a1cee2f95ac4215c5
Felipe Barros <felipetech@tuta.io>
2020-10-30 18:09:37 -0300

    test(server): Cover uncovered lines in the UploadsConfigService tests

--- server/src/uploads/uploads-config.service.spec.ts
+++ server/src/uploads/uploads-config.service.spec.ts
@@ -1,22 +1,24 @@
-// TODO: Cover uncovered lines
-import { Test, TestingModule } from '@nestjs/testing';
+import { Readable } from 'stream';
 import { UploadsConfigService } from './uploads-config.service';
-
-const uploadsConfigServiceMock = () => ({
-  createMulterOptions: jest.fn(),
-});
+import { UnprocessableEntityException } from '@nestjs/common';
 
 describe('UploadsConfigService', () => {
-  let service: any;
+  const service = new UploadsConfigService();
+  let fileMock: Express.Multer.File;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        { provide: UploadsConfigService, useFactory: uploadsConfigServiceMock },
-      ],
-    }).compile();
-
-    service = module.get<UploadsConfigService>(UploadsConfigService);
+  beforeEach(() => {
+    fileMock = {
+      path: 'any_path',
+      size: 1,
+      buffer: ('any_buffer' as unknown) as Buffer,
+      fieldname: 'any_fieldname',
+      stream: ('any_stream' as unknown) as Readable,
+      encoding: 'any_encoding',
+      filename: 'any_filename',
+      mimetype: 'any_mimetype',
+      destination: 'any_destination',
+      originalname: 'any_originalname',
+    };
   });
 
   it('should be defined', () => {
@@ -24,12 +26,81 @@ describe('UploadsConfigService', () => {
   });
 
   describe('createMulterOptions()', () => {
-    it('should call createMulterOptions() and return the result', () => {
-      const expectedResponse = 'any_value';
-      service.createMulterOptions.mockReturnValue(expectedResponse);
-      const result = service.createMulterOptions();
+    describe('fileFilter()', () => {
+      it('should call createMulterOptions() and return the fileFilter property', () => {
+        const result = service.createMulterOptions();
+        expect(result).toHaveProperty('fileFilter');
+      });
 
-      expect(result).toEqual(expectedResponse);
+      it('should call the fail callback', () => {
+        const result = service.createMulterOptions();
+
+        const req = {};
+        const callback = jest.fn();
+        result.fileFilter(req, fileMock, callback);
+
+        expect(callback).toBeCalledWith(
+          new UnprocessableEntityException('Invalid file type'),
+          false,
+        );
+      });
+
+      it('should call the success callback', () => {
+        const result = service.createMulterOptions();
+
+        const validMimeType = 'image/jpeg';
+        const req = {};
+        fileMock.mimetype = validMimeType;
+        const callback = jest.fn();
+        result.fileFilter(req, fileMock, callback);
+
+        expect(callback).toBeCalledWith(null, true);
+      });
+    });
+
+    describe('limits', () => {
+      it('should call createMulterOptions() and return the limits property', () => {
+        const result = service.createMulterOptions();
+        expect(result).toHaveProperty('limits');
+      });
+
+      it('should have the fileSize property', () => {
+        const result = service.createMulterOptions();
+
+        expect(result.limits).toHaveProperty('fileSize');
+      });
+    });
+
+    describe('storage', () => {
+      it('should call createMulterOptions() and return the storage property', () => {
+        const result = service.createMulterOptions();
+
+        expect(result).toHaveProperty('storage');
+      });
+
+      it('should call getFilename()', () => {
+        const multerOptions = service.createMulterOptions();
+
+        const req = {};
+        const file = fileMock;
+        const callback = jest.fn();
+
+        multerOptions.storage.getFilename(req, file, callback);
+
+        expect(callback).toBeCalled();
+      });
+
+      it('should call getDestination()', () => {
+        const multerOptions = service.createMulterOptions();
+
+        const req = {};
+        const file = fileMock;
+        const callback = jest.fn();
+
+        multerOptions.storage.getDestination(req, file, callback);
+
+        expect(callback).toBeCalled();
+      });
     });
   });
 });
